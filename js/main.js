f94faf08e8a49df4103cc9b9f9b614c97c295911
TanishqSundhan <mail@tanishqsundhan.com>
2021-08-22 10:23:50 +0530

    added cookie banner

--- js/main.js
+++ js/main.js
@@ -131,3 +131,75 @@
 
 })(jQuery);
 
+
+
+// Cookie Popup
+
+$(document).ready(function(){
+    if(readCookie("cookie_accepted") == "1"){
+       $(".cookie-bar").hide();
+   }
+   else{
+       $(".cookie-bar").show();
+           $('body').addClass('cookie-space');
+           $('.cookie-btn').click(function(){
+               $('body').removeClass('cookie-space');
+           $('.cookie-bar').fadeOut();
+           createCookie("cookie_accepted", 1, 365);
+       });     
+   } 
+   });
+
+
+
+function getParameterByName(name, url) {
+       if (!url) {
+           url = window.location.href;
+       }
+       name = name.replace(/[\[\]]/g, "\\$&");
+       var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+           results = regex.exec(url);
+       if (!results) return null;
+       if (!results[2]) return '';
+       return decodeURIComponent(results[2].replace(/\+/g, " "));
+   }
+
+   function createCookie(name, value, days) {
+       var expires = "";
+       if (days) {
+           var date = new Date();
+           date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+           expires = "; expires=" + date.toUTCString();
+       }
+       document.cookie = name + "=" + value + expires + "; path=/";
+   }
+
+   function readCookie(name) {
+       var nameEQ = name + "=";
+       var ca = document.cookie.split(';');
+       for (var i = 0; i < ca.length; i++) {
+           var c = ca[i];
+           while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+           if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+       }
+       return null;
+   }
+
+   function eraseCookie(name) {
+       createCookie(name, "", -1);
+   }
+
+   $(document).ready(function() {
+       var advMedium = getParameterByName('advm');
+       if (advMedium != null) {
+           $('input[name=advm]').val(advMedium);
+           createCookie('advm', advMedium, 1);
+       } else {
+           advMedium = readCookie('advm');
+           $('input[name=advm]').val(advMedium);
+       }
+       var nodeCount = document.getElementsByName('ft').length;
+       for (count = 0; count < nodeCount; count++) {
+           document.getElementsByName('ft')[count].value = window.location.href;
+       }
+   });
